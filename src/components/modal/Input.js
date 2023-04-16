846e297cf826489f5b3190a9fa9704f17ebcb4c9
Nizar Mayraldo <nizar.aldo@yahoo.com>
2023-04-16 16:57:00 +0700

    create item

---
+++ src/components/modal/Input.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
+import Stack from '@mui/material/Stack';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {Typography, FormControl, Paper} from "@mui/material";
+
+const Modal = ({handleCloseModal, list, addList}) => {
+
+    const theme = createTheme({
+        components: {
+          MuiButton: {
+            variants: [
+              {
+                props: { variant: 'primary' },
+                style: {
+                  color: '#F0F9FF',
+              padding: '8px 16px',
+
+                  textTransform: 'none',
+                  fontStyle: 'normal',
+                  fontSize: '14px',          
+                  backgroundColor: '#075985',
+                  borderRadius: '8px',
+                  '&:hover': {
+                    backgroundColor: '#075985',
+                   color: '#F0F9FF',
+                  },
+                },
+              },
+              {
+                props: { variant: 'secondary'}, 
+                style: {
+                  color: '#0369A1',
+                  textTransform: 'none',
+                  backgroundColor: '#F5F5F5',
+                  fontWeight: 600,
+                  fontSize: '14px',          
+                  '&:hover': {
+                    backgroundColor: '#F5F5F5',
+                   color: '#0369A1',
+                  },
+                },
+              },
+            ],
+          },
+        },
+      });
+
+      const [input, setInput] = useState('')
+      const [error, setError] = useState(false)
+
+      const handleSubmit = (e) => {
+        if(input === '') {
+            setError(true)
+        }
+        else {
+            e.preventDefault()
+            addList(input)
+            setInput('')
+            handleCloseModal()
+        }
+      }
+
+      const handleInputChange = (e) => {
+        const cek = e.target.value
+        setInput(cek)
+        setError(cek.length > 100)
+      }
+
+    return <ThemeProvider theme={theme}>
+        <Grid item xs={2} >
+        </Grid>
+        <Grid item xs={7}>
+            <Paper elevation={0} sx={{backgroundColor:'#F5F5F5'}}>
+                <Input 
+                placeholder="Add new to-do title..." 
+                disableUnderline={true} 
+                multiline={true}
+                sx={{display:'block', marginLeft:'8%', fontStyle: 'normal', fontWeight: '400', fontSize: '16px'}}
+                value={input}
+                onChange={handleInputChange}
+                error={error}
+                />
+            </Paper>
+            {error && (
+                <Typography variant='caption' sx={{color:'#E11D48', textAlign:'left', marginLeft: '50px', display:'flex'}}>
+                    Title must be shorter than or equal to 100 characters.
+                </Typography>
+            )}
+        </Grid>
+        <Grid item xs={3} >
+            <Stack direction="row" spacing={2}>
+                <Button variant='secondary' onClick={handleCloseModal}>Cancel</Button>
+                <Button variant='primary' onClick={handleSubmit}>Create</Button>
+            </Stack>
+        </Grid>
+    </ThemeProvider>
+}
+
+export default Modal
---
+++ src/page/Dashboard.js
@@ -0,0 +1,142 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Stack from '@mui/material/Stack';
+import Modal from '../components/modal/Input.js';
+
+const Dasboard = () => {
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#F5F5F5',
+    fontFamily: 'Calibry',
+    fontWeight: 700,
+    fontSize: '24px',
+    textAlign: 'center',
+    color: '#292524',
+    lineHeight: '28px'
+  }));
+
+  const theme = createTheme({
+    components: {
+      MuiButton: {
+        variants: [
+          {
+            props: { variant: 'primary' },
+            style: {
+              color: '#F0F9FF',
+              padding: '8px 16px',
+              textTransform: 'none',
+              fontStyle: 'normal',
+              fontSize: '14px',          
+              backgroundColor: '#075985',
+              borderRadius: '8px',
+              '&:hover': {
+                backgroundColor: '#075985',
+               color: '#F0F9FF',
+              },
+            },
+          },
+          {
+            props: { variant: 'modal'}, 
+            style: {
+              color: '#F5F5F5',
+              opacity: '0',
+              textTransform: 'none',
+              backgroundColor: '#F5F5F5',
+              '&:hover': {
+                backgroundColor: '#F5F5F5',
+                color: '#F5F5F5'
+              },
+            },
+          },
+          {
+            props: { variant: 'cancel'},
+            style: {
+              color:'#BE123C',
+              textTransform: 'none',
+              fontStyle: 'normal',
+              fontWeight: 600,
+              fontSize: '14px',          
+              '&:hover': {
+                backgroundColor: '#F5F5F5',
+               color: '#BE123C',
+              },
+            },
+          },
+        ],
+      },
+    },
+  });
+  
+  const [list,setList] = useState([])
+  const [modal,setModal] = useState(false)
+
+  const handleCloseModal = () => {
+    setModal(false);
+  }
+
+  const addList = (text) => {
+    const newList = [...list, {text}]
+    setList(newList)
+  }
+  console.log(list);
+  return (
+      <Container fixed sx={{py: '20px'}}>
+        <Box>
+          <Grid container spacing={3} py={4}>
+            <Grid item xs={9}>
+              <Item elevation={0}>Things you should be doing today...</Item>
+            </Grid>
+            <Grid item xs={3}>
+            <Stack direction="row" spacing={2}>
+              <ThemeProvider theme={theme}>
+                {modal ? (
+                  <Button variant='modal' disabled>Add New</Button>
+                ) 
+                : (
+                <Button variant='primary' onClick={()=>setModal(true)}>Add New</Button>
+                )}
+                <Button variant='cancel' onClick={setList([])}>Clear</Button>
+              </ThemeProvider>
+            </Stack>
+            </Grid>
+            {modal ?
+            (
+              <Modal handleCloseModal={handleCloseModal} list={list} addList={addList}/>
+            )
+            :
+            <></>
+          }
+          { list.length != 0 ? (
+                <></>
+            )
+            :(<>
+                <Grid item xs={2.5}>
+            </Grid>
+            <Grid item xs={8.5}>
+                <Paper sx={{paddingY: "50px", 
+                    fontStyle: 'normal', 
+                    fontWeight: '500', 
+                    fontSize: '14px'}}
+                    >
+                Nothing to-do yet.
+                </Paper>
+            </Grid>
+            <Grid item xs={1}>
+            </Grid>
+            </>
+            )
+        }
+        </Grid>
+        </Box>
+      </Container>
+  );
+}
+
+export default Dasboard;
